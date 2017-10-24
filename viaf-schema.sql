c1e80c00efb5ef9e563bb54e737bbbb9520818ea
Michael Ekstrand <md@ekstrandom.net>
2017-10-24 15:45:29 -0600

    select import

--- viaf-schema.sql
+++ viaf-schema.sql
@@ -1,11 +1,12 @@
 DROP TABLE IF EXISTS viaf_author CASCADE;
 CREATE TABLE viaf_author (
-  viaf_au_id BIGINT PRIMARY KEY
+  viaf_au_id SERIAL PRIMARY KEY,
+  viaf_au_key VARCHAR NOT NULL UNIQUE
 );
 
 DROP TABLE IF EXISTS viaf_author_name CASCADE;
 CREATE TABLE viaf_author_name (
-  viaf_au_id BIGINT NOT NULL,
+  viaf_au_id INTEGER NOT NULL,
   viaf_au_name VARCHAR NOT NULL,
   viaf_au_name_type VARCHAR(1) NULL,
   viaf_au_name_dates VARCHAR NULL,
@@ -14,7 +15,7 @@ CREATE TABLE viaf_author_name (
 
 DROP TABLE IF EXISTS viaf_author_gender CASCADE;
 CREATE TABLE viaf_author_gender (
-  viaf_au_id BIGINT NOT NULL,
+  viaf_au_id INTEGER NOT NULL,
   viaf_au_gender VARCHAR NOT NULL,
   viaf_au_gender_start VARCHAR NULL,
   viaf_au_gender_end VARCHAR NULL,
