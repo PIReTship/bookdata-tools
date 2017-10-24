7f74b66f9587b759af6754ada4aee2f130891262
Michael Ekstrand <md@ekstrandom.net>
2017-10-24 15:19:06 -0600

    imports and names

--- viaf-schema.sql
+++ viaf-schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE viaf_author (
   viaf_au_id BIGINT PRIMARY KEY
 );
 
-DROP TABLE IF EXISTS viaf_author CASCADE;
+DROP TABLE IF EXISTS viaf_author_name CASCADE;
 CREATE TABLE viaf_author_name (
   viaf_au_id BIGINT NOT NULL,
   viaf_au_name VARCHAR NOT NULL,
@@ -12,7 +12,7 @@ CREATE TABLE viaf_author_name (
   viaf_au_name_source VARCHAR NULL
 );
 
-DROP TABLE IF EXISTS viaf_author CASCADE;
+DROP TABLE IF EXISTS viaf_author_gender CASCADE;
 CREATE TABLE viaf_author_gender (
   viaf_au_id BIGINT NOT NULL,
   viaf_au_gender VARCHAR NOT NULL,
