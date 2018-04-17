226c07ef35ded0de69d3049bda9b0824b39ff244
Michael Ekstrand <michaelekstrand@boisestate.edu>
2018-04-17 09:04:45 -0600

    VIAF record index

--- viaf-index.sql
+++ viaf-index.sql
@@ -1,12 +1,14 @@
-CREATE INDEX viaf_author_name_id_idx ON viaf_author_name (viaf_au_id);
-CREATE INDEX viaf_author_name_idx ON viaf_author_name (viaf_au_name);
-ALTER TABLE viaf_author_name ADD CONSTRAINT viaf_au_name_fk FOREIGN KEY (viaf_au_id) REFERENCES viaf_author;
+CREATE INDEX viaf_marc_field_rec_idx ON viaf_marc_field (rec_id);
 
-DELETE FROM viaf_author_name WHERE viaf_au_name_source = 'SYNTH';
-INSERT INTO viaf_author_name (viaf_au_id, viaf_au_name, viaf_au_name_source, viaf_au_name_dates)
-SELECT viaf_au_id, regexp_replace(regexp_replace(viaf_au_name, ',$', ''), '^(.*), (.*)', '\2 \1'), 'SYNTH', viaf_au_name_dates
-FROM viaf_author_name
-WHERE viaf_au_name LIKE '%,%';
+-- CREATE INDEX viaf_author_name_id_idx ON viaf_author_name (viaf_au_id);
+-- CREATE INDEX viaf_author_name_idx ON viaf_author_name (viaf_au_name);
+-- ALTER TABLE viaf_author_name ADD CONSTRAINT viaf_au_name_fk FOREIGN KEY (viaf_au_id) REFERENCES viaf_author;
 
-CREATE INDEX viaf_gender_id_idx ON viaf_author_gender (viaf_au_id);
-ALTER TABLE viaf_author_gender ADD CONSTRAINT viaf_au_id_fk FOREIGN KEY (viaf_au_id) REFERENCES viaf_author;
+-- DELETE FROM viaf_author_name WHERE viaf_au_name_source = 'SYNTH';
+-- INSERT INTO viaf_author_name (viaf_au_id, viaf_au_name, viaf_au_name_source, viaf_au_name_dates)
+-- SELECT viaf_au_id, regexp_replace(regexp_replace(viaf_au_name, ',$', ''), '^(.*), (.*)', '\2 \1'), 'SYNTH', viaf_au_name_dates
+-- FROM viaf_author_name
+-- WHERE viaf_au_name LIKE '%,%';
+
+-- CREATE INDEX viaf_gender_id_idx ON viaf_author_gender (viaf_au_id);
+-- ALTER TABLE viaf_author_gender ADD CONSTRAINT viaf_au_id_fk FOREIGN KEY (viaf_au_id) REFERENCES viaf_author;
