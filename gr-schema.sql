78a998ca4b007e784cef1d50405809f0d37dd0f7
Michael Ekstrand <md@ekstrandom.net>
2018-11-16 10:02:27 -0700

    Update GR schema for raw naming

--- gr-schema.sql
+++ gr-schema.sql
@@ -1,16 +1,16 @@
-CREATE TABLE gr_interaction (
+CREATE TABLE gr_interaction_raw (
   gr_int_rid SERIAL NOT NULL,
   gr_int_data JSONB NOT NULL
 );
-CREATE TABLE gr_book (
+CREATE TABLE gr_book_raw (
   gr_book_rid SERIAL NOT NULL,
   gr_book_data JSONB NOT NULL
 );
-CREATE TABLE gr_work (
+CREATE TABLE gr_work_raw (
   gr_work_rid SERIAL NOT NULL,
   gr_work_data JSONB NOT NULL
 );
-CREATE TABLE gr_author (
+CREATE TABLE gr_author_raw (
   gr_author_rid SERIAL NOT NULL,
   gr_author_data JSONB NOT NULL
 );
