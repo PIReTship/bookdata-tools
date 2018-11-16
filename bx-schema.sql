8edfaa0c5da77a6e2d936373c51bc4e41e512219
Michael Ekstrand <michaelekstrand@boisestate.edu>
2018-11-15 17:29:21 -0700

    fix bx schema

--- bx-schema.sql
+++ bx-schema.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS az_raw_ratings CASCADE;
-CREATE TABLE az_raw_ratings (
+DROP TABLE IF EXISTS bx_raw_ratings CASCADE;
+CREATE TABLE bx_raw_ratings (
   user_id INTEGER NOT NULL,
   isbn VARCHAR NOT NULL,
   rating REAL NOT NULL
