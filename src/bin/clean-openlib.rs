4bf05a9e723af1a007cdfdc96b191fe9640ae871
Michael Ekstrand <md@ekstrandom.net>
2018-12-04 14:39:09 -0700

    support stdin for cleaning openlib

--- src/bin/clean-openlib.rs
+++ src/bin/clean-openlib.rs
@@ -17,26 +17,42 @@ use bookdata::tsv::split_first;
 #[structopt(name="clean-openlib")]
 struct Opt {
   #[structopt(name = "FILE", parse(from_os_str))]
-  infile: PathBuf
+  infile: Option<PathBuf>
 }
 
-fn main() -> io::Result<()> {
-  let opt = Opt::from_args();
-  let mut fs = File::open(opt.infile)?;
-  let mut gzf = GzDecoder::new(fs);
-  let mut bfs = BufReader::new(gzf);
-  let stdout = io::stdout();
-  let mut oul = stdout.lock();
-  for line in bfs.lines() {
+fn process<R: BufRead, W: Write>(src: &mut R, dst: &mut W) -> io::Result<()> {
+  for line in src.lines() {
     let ls = line?;
     let (_ty, rest) = split_first(&ls).expect("bad line");
     let (key, rest) = split_first(rest).expect("bad line");
     let (_ver, rest) = split_first(rest).expect("bad line");
     let (_stamp, json) = split_first(rest).expect("bad line");
-    oul.write_all(key.as_bytes())?;
-    oul.write_all(b"\t")?;
-    write_encoded(&mut oul, json.as_bytes())?;
-    oul.write_all(b"\n")?;
+    dst.write_all(key.as_bytes())?;
+    dst.write_all(b"\t")?;
+    write_encoded(dst, json.as_bytes())?;
+    dst.write_all(b"\n")?;
+  }
+
+  Ok(())
+}
+
+fn main() -> io::Result<()> {
+  let opt = Opt::from_args();
+  let stdout = io::stdout();
+  let mut out = stdout.lock();
+
+  match opt.infile {
+    Some(f) => {
+      let mut fs = File::open(f)?;
+      let mut gzf = GzDecoder::new(fs);
+      let mut bfs = BufReader::new(gzf);
+      process(&mut bfs, &mut out)?;
+    },
+    None => {
+      let si = io::stdin();
+      let mut src = si.lock();
+      process(&mut src, &mut out)?;
+    }
   }
 
   Ok(())
