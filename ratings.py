0f1b446d11b625168ae6aa5382bf0df9dbbb5d79
Michael Ekstrand <md@ekstrandom.net>
2018-12-05 13:08:13 -0700

    add import status state support

--- ratings.py
+++ ratings.py
@@ -11,9 +11,10 @@ from invoke import task
 import support as s
 
 
-@task
-def import_bx(c):
+@task(s.init)
+def import_bx(c, force=False):
     "Import BookCrossing ratings"
+    s.start('bx-ratings', force=force)
     print("initializing BX schema")
     c.run('psql -f bx-schema.sql')
     print("cleaning BX rating data")
@@ -31,20 +32,20 @@ def import_bx(c):
     print('importing BX to database')
     data = bytes(barr)
     rd = StringIO(data.decode('utf8'))
-    dbc = psycopg2.connect("")
-    try:
+
+    with s.database() as dbc:
         # with dbc encapsulates a transaction
         with dbc, dbc.cursor() as cur:
             for row in tqdm(csv.DictReader(rd)):
                 cur.execute('INSERT INTO bx_raw_ratings (user_id, isbn, rating) VALUES (%s, %s, %s)',
                             (row['User-ID'], row['ISBN'], row['Book-Rating']))
-    finally:
-        dbc.close()
+            s.finish('bx-ratings', dbc)
 
 
-@task(s.build)
-def import_az(c):
+@task(s.init, s.build)
+def import_az(c, force=False):
     "Import Amazon ratings"
+    s.start('az-ratings', force=force)
     print('Resetting Amazon schema')
     c.run('psql -f az-schema.sql')
     print('Importing Amazon ratings')
@@ -52,11 +53,13 @@ def import_az(c):
       [s.bin_dir / 'pcat', s.data_dir / 'ratings_Books.csv'],
       ['psql', '-c', '\\copy az_raw_ratings FROM STDIN (FORMAT CSV)']
     ])
+    s.finish('bx-ratings')
 
 
-@task(s.build)
-def import_gr(c):
+@task(s.init, s.build)
+def import_gr(c, force=False):
     "Import GoodReads ratings"
+    s.start('gr-data', force=force)
     print('Resetting GoodReads schema')
     c.run('psql -f gr-schema.sql')
     print('Importing GoodReads books')
@@ -79,3 +82,4 @@ def import_gr(c):
       [s.bin_dir / 'clean-json', s.data_dir / 'goodreads_interactions.json.gz'],
       ['psql', '-c', '\\copy gr_raw_interaction (gr_int_data) FROM STDIN']
     ])
+    s.finish('gr-data')
--- support.py
+++ support.py
@@ -1,11 +1,19 @@
+import sys
 import os
 from pathlib import Path
 import subprocess as sp
 from invoke import task
+import psycopg2
 
 data_dir = Path('data')
 tgt_dir = Path('target')
 bin_dir = tgt_dir / 'release'
+
+@task
+def init(c):
+    "Make sure initial database structure are in place"
+    c.run("psql -f common-schema.sql")
+
 
 @task
 def build(c, debug=False):
@@ -41,3 +49,78 @@ def pipeline(steps, outfile=None):
         if rc != 0:
             print(f'{s[0]} exited with code {rc}', file=sys.stderr)
             raise RuntimeError('subprocess failed')
+
+
+class database:
+    def __init__(self, autocommit = False, dbc=None):
+        self.autocommit = autocommit
+        self.connection = dbc
+        self.need_close = False
+
+    def __enter__(self):
+        if self.connection is None:
+            self.connection = psycopg2.connect("")
+            self.need_close = True
+
+            if self.autocommit:
+                self.connection.set_session(autocommit=True)
+
+        return self.connection
+    
+    def __exit__(self, *args):
+        if self.need_close:
+            self.connection.close()
+            self.need_close = False
+
+
+def check_prereq(step, dbc=None):
+    with database(dbc=dbc, autocommit=True) as db:
+        with db.cursor() as cur:
+            cur.execute('''
+                SELECT finished_at FROM import_status 
+                WHERE step = %s AND finished_at IS NOT NULL
+            ''', [step])
+            res = cur.fetchone()
+            if not res:
+                print('prerequisite step', step, 'not completed', file=sys.stderr)
+                raise RuntimeError('prerequisites not met')
+
+
+def start(step, force=False, dbc=None):
+    with database(dbc=dbc, autocommit=True) as db:
+        with db.cursor() as cur:
+            cur.execute('''
+                SELECT finished_at FROM import_status 
+                WHERE step = %s
+            ''', [step])
+            res = cur.fetchone()
+            if res:
+                date, = res
+                if date:
+                    print('step {} already completed at {}'.format(step, date),
+                          file=sys.stderr)
+                    if force:
+                        print('continuing anyway', file=sys.stderr)
+                    else:
+                        raise RuntimeError('step {} already completed'.format(step))
+                else:
+                    print('WARNING: step', step, 'already started, did it fail?')
+            cur.execute('''
+                INSERT INTO import_status (step)
+                VALUES (%s)
+                ON CONFLICT (step)
+                DO UPDATE started_at = now(), finished_at = NULL
+            ''', [step])
+
+
+def finish(step, dbc=None):
+    with database(dbc=dbc, autocommit=True) as db:
+        with db.cursor() as cur:
+            cur.execute('''
+                UPDATE import_status
+                SET finished_at = now()
+                WHERE step = %s
+            ''', [step])
+            ct = cur.rowcount
+            if ct != 1:
+                raise RuntimeError("couldn't update step!")
