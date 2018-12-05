fcb3b3403e662e28e79c46af463069e10c894d13
Michael Ekstrand <md@ekstrandom.net>
2018-12-05 09:23:00 -0700

    more erorr handling

--- src/bin/parse-marc.rs
+++ src/bin/parse-marc.rs
@@ -15,6 +15,7 @@ use quick_xml::events::Event;
 use flate2::bufread::MultiGzDecoder;
 use indicatif::{ProgressBar, ProgressStyle};
 
+use bookdata::{Result, err};
 use bookdata::cleaning::write_pgencoded;
 use bookdata::tsv::split_first;
 
@@ -38,30 +39,29 @@ struct Field<'a> {
 
 /// Process a tab-delimited line file.  VIAF provides their files in this format;
 /// each line is a tab-separated pair of the VIAF ID and a single `record` instance.
-fn process_delim_file<R: BufRead, W: Write>(r: &mut R, w: &mut W, init: usize) -> io::Result<usize> {
-  let mut count = init;
+fn process_delim_file<R: BufRead, W: Write>(r: &mut R, w: &mut W, init: usize) -> Result<usize> {
+  let mut rec_count = 0;
   for line in r.lines() {
     let lstr = line?;
-    let (_id, xml) = split_first(&lstr).expect("invalid line");
+    let (_id, xml) = split_first(&lstr).ok_or(err("invalid line"))?;
     let mut parse = Reader::from_str(xml);
-    let old = count;
-    process_records(&mut parse, w, &mut count);
+    let n = process_records(&mut parse, w, init + rec_count)?;
     // we should only have one record per file
-    assert_eq!(count, old+1);
+    assert_eq!(n, 1);
+    rec_count += n;
   }
 
-  Ok(count)
+  Ok(rec_count)
 }
 
 /// Process a file containing a MARC collection.
-fn process_marc_file<R: BufRead, W: Write>(r: &mut R, w: &mut W, init: usize) -> io::Result<usize> {
-  let mut count = init;
+fn process_marc_file<R: BufRead, W: Write>(r: &mut R, w: &mut W, init: usize) -> Result<usize> {
   let mut parse = Reader::from_reader(r);
-  process_records(&mut parse, w, &mut count);
+  let count = process_records(&mut parse, w, init)?;
   Ok(count)
 }
 
-fn write_codes<W: Write>(w: &mut W, rno: usize, fno: i32, tag: &[u8], fld: Option<&Field>) -> io::Result<()> {
+fn write_codes<W: Write>(w: &mut W, rno: usize, fno: i32, tag: &[u8], fld: Option<&Field>) -> Result<()> {
   let ids = format!("{}\t{}\t", rno, fno);
   w.write_all(ids.as_str().as_bytes())?;
   w.write_all(tag)?;
@@ -86,33 +86,34 @@ fn write_nl<W: Write>(w: &mut W) -> io::Result<()> {
   w.write_all(b"\n")
 }
 
-fn process_records<B: BufRead, W: Write>(rdr: &mut Reader<B>, out: &mut W, lno: &mut usize) {
+fn process_records<B: BufRead, W: Write>(rdr: &mut Reader<B>, out: &mut W, start: usize) -> Result<usize> {
   let mut buf = Vec::new();
   let mut output = false;
   let mut fno = 0;
   let mut tag = Vec::with_capacity(5);
   let mut ind1 = Vec::with_capacity(10);
   let mut ind2 = Vec::with_capacity(10);
+  let mut recid = start;
   loop {
-    match rdr.read_event(&mut buf) {
-      Ok(Event::Start(ref e)) => {
-        let name = str::from_utf8(e.local_name()).unwrap();
+    match rdr.read_event(&mut buf)? {
+      Event::Start(ref e) => {
+        let name = str::from_utf8(e.local_name())?;
         match name {
           "record" => {
-            *lno += 1
+            recid += 1
           },
           "leader" => {
-            write_codes(out, *lno, fno, b"LDR", None).expect("output error");
+            write_codes(out, recid, fno, b"LDR", None)?;
             output = true;
           },
           "controlfield" => {
             fno += 1;
             let mut done = false;
             for ar in e.attributes() {
-              let a = ar.expect("decode error");
+              let a = ar?;
               if a.key == b"tag" {
-                let tag = a.unescaped_value().expect("decode error");
-                write_codes(out, *lno, fno, &tag, None).expect("output error");
+                let tag = a.unescaped_value()?;
+                write_codes(out, recid, fno, &tag, None)?;
                 done = true;
               }
             }
@@ -122,8 +123,8 @@ fn process_records<B: BufRead, W: Write>(rdr: &mut Reader<B>, out: &mut W, lno: 
           "datafield" => {
             fno += 1;
             for ar in e.attributes() {
-              let a = ar.expect("decode error");
-              let v = a.unescaped_value().expect("decode error");
+              let a = ar?;
+              let v = a.unescaped_value()?;
               match a.key {
                 b"tag" => tag.extend_from_slice(&*v),
                 b"ind1" => ind1.extend_from_slice(&*v),
@@ -138,11 +139,11 @@ fn process_records<B: BufRead, W: Write>(rdr: &mut Reader<B>, out: &mut W, lno: 
           "subfield" => {
             let mut done = false;
             for ar in e.attributes() {
-              let a = ar.expect("decode error");
+              let a = ar?;
               if a.key == b"code" {
-                let code = a.unescaped_value().expect("decode error");
+                let code = a.unescaped_value()?;
                 let field = Field { ind1: &ind1, ind2: &ind2, code: &code };
-                write_codes(out, *lno, fno, &tag, Some(&field)).expect("output error");
+                write_codes(out, recid, fno, &tag, Some(&field))?;
                 done = true;
               }
             }
@@ -152,11 +153,11 @@ fn process_records<B: BufRead, W: Write>(rdr: &mut Reader<B>, out: &mut W, lno: 
           _ => ()
         }
       },
-      Ok(Event::End(ref e)) => {
-        let name = str::from_utf8(e.local_name()).unwrap();
+      Event::End(ref e) => {
+        let name = str::from_utf8(e.local_name())?;
         match name {
           "leader" | "controlfield" | "subfield" => {
-            write_nl(out).expect("output error");
+            write_nl(out)?;
             output =  false;
           },
           "datafield" => {
@@ -167,20 +168,20 @@ fn process_records<B: BufRead, W: Write>(rdr: &mut Reader<B>, out: &mut W, lno: 
           _ => ()
         }
       },
-      Ok(Event::Text(e)) => {
+      Event::Text(e) => {
         if output {
-          let t = e.unescaped().expect("decode error");
-          write_pgencoded(out, &t).expect("output error")
+          let t = e.unescaped()?;
+          write_pgencoded(out, &t)?
         }
       },
-      Ok(Event::Eof) => break,
-      Err(e) => panic!("Error at position {}: {:?}", rdr.buffer_position(), e),
+      Event::Eof => break,
       _ => ()
     }
   }
+  Ok(recid - start)
 }
 
-fn main() -> io::Result<()> {
+fn main() -> Result<()> {
   let opt = Opt::from_args();
   let out = io::stdout();
   let mut outlock = out.lock();
@@ -197,12 +198,20 @@ fn main() -> io::Result<()> {
     let gzf = MultiGzDecoder::new(pbr);
     let mut bfs = BufReader::new(gzf);
     let nrecs = if opt.linemode {
-      process_delim_file(&mut bfs, &mut outlock, count)?
+      process_delim_file(&mut bfs, &mut outlock, count)
     } else {
-      process_marc_file(&mut bfs, &mut outlock, count)?
+      process_marc_file(&mut bfs, &mut outlock, count)
     };
-    eprintln!("processed {} records from {:?}", nrecs, inf);
-    count += nrecs;
+    match nrecs {
+      Ok(n) => {
+        eprintln!("processed {} records from {:?}", n, inf);
+        count += n;
+      },
+      Err(e) => {
+        eprintln!("error in {:?}: {}", inf, e);
+        return Err(e)
+      }
+    }
   }
   Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,47 @@
+extern crate quick_xml;
+
 pub mod cleaning;
 pub mod tsv;
+
+use std::io;
+use std::fmt;
+use std::error::Error;
+
+#[derive(Debug)]
+pub enum BDError {
+  IO(io::Error),
+  XML(quick_xml::Error),
+  UTF8(std::str::Utf8Error),
+  Misc(String)
+}
+
+impl fmt::Display for BDError {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "{:?}", self)
+  }
+}
+
+impl From<io::Error> for BDError {
+  fn from(err: io::Error) -> BDError {
+    BDError::IO(err)
+  }
+}
+
+impl From<std::str::Utf8Error> for BDError {
+  fn from(err: std::str::Utf8Error) -> BDError {
+    BDError::UTF8(err)
+  }
+}
+
+impl From<quick_xml::Error> for BDError {
+  fn from(err: quick_xml::Error) -> BDError {
+    BDError::XML(err)
+  }
+}
+
+pub fn err(msg: &str) -> BDError {
+  BDError::Misc(msg.to_string())
+}
+
+/// Typedef for all-purpose result type.
+pub type Result<R> = std::result::Result<R, BDError>;
