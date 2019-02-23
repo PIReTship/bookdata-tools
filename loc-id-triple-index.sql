d61109fe1d46888dbffd44890c9ebf8fd8352032
Michael Ekstrand <md@ekstrandom.net>
2019-02-23 11:01:45 -0700

    Report statement time in triple index

--- loc-id-triple-index.sql
+++ loc-id-triple-index.sql
@@ -1,27 +1,43 @@
-BEGIN TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
-
-DO $$ BEGIN
+DO $$ 
+DECLARE
+  st_time timestamp := now();
+BEGIN
   RAISE NOTICE 'Indexing node IRIs' USING TABLE = 'locid.nodes';
-END; $$;
-CREATE INDEX IF NOT EXISTS node_iri_idx ON locid.nodes (node_iri);
-ANALYZE locid.nodes;
+  CREATE INDEX IF NOT EXISTS node_iri_idx ON locid.nodes (node_iri);
+  ANALYZE locid.nodes;
+  RAISE NOTICE 'Indexed node IRIs in %', now() - st_time;
+END; 
+$$;
 
-DO $$ BEGIN
+DO $$ 
+DECLARE
+  st_time timestamp := now();
+BEGIN
   RAISE NOTICE 'Adding literal PK' USING TABLE = 'locid.literals';
-END; $$;
-ALTER TABLE locid.literals ADD CONSTRAINT literal_pkey PRIMARY KEY (lit_id);
-ANALYZE locid.literals;
+  ALTER TABLE locid.literals ADD CONSTRAINT literal_pkey PRIMARY KEY (lit_id);
+  ANALYZE locid.literals;
+  RAISE NOTICE 'Added literal PK in %', now() - st_time;
+END;
+$$;
 
-DO $$ BEGIN
+DO $$
+DECLARE
+  st_time timestamp := now();
+BEGIN
   RAISE NOTICE 'Indexing authority subjects and objects' USING TABLE = 'locid.auth_triple';
-END; $$;
-CREATE INDEX IF NOT EXISTS auth_subject_idx ON locid.auth_triple (subject_id);
-CREATE INDEX IF NOT EXISTS auth_object_idx ON locid.auth_triple (object_id);
+  CREATE INDEX IF NOT EXISTS auth_subject_idx ON locid.auth_triple (subject_id);
+  CREATE INDEX IF NOT EXISTS auth_object_idx ON locid.auth_triple (object_id);
+  RAISE NOTICE 'Indexed authority table in %', now() - st_time;
+END;
+$$;
 
-DO $$ BEGIN
+DO $$
+DECLARE
+  st_time timestamp := now();
+BEGIN
   RAISE NOTICE 'Indexing BIBFRAME work subjects and objects' USING TABLE = 'locid.work_triple';
-END; $$;
-CREATE INDEX IF NOT EXISTS work_subject_idx ON locid.work_triple (subject_id);
-CREATE INDEX IF NOT EXISTS work_object_idx ON locid.work_triple (object_id);
-
-COMMIT;
+  CREATE INDEX IF NOT EXISTS work_subject_idx ON locid.work_triple (subject_id);
+  CREATE INDEX IF NOT EXISTS work_object_idx ON locid.work_triple (object_id);
+  RAISE NOTICE 'Indexed BIBFRAME work table in %', now() - st_time;
+END;
+$$;
