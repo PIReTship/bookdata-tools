aaabf4bef685f069252ff780ff1caa0a1b2c84e5
Michael Ekstrand <md@ekstrandom.net>
2019-03-01 17:21:11 -0700

    more loc-id stuff

--- loc-id-auth-index.sql
+++ loc-id-auth-index.sql
@@ -1,13 +1,46 @@
 --- #step Create auth labels
-CREATE MATERIALIZED VIEW IF NOT EXISTS locid.auth_node_label AS
-SELECT subject_id, lit_value AS label
+CREATE TABLE IF NOT EXISTS locid.auth_node_label (
+  subject_id UUID NOT NULL,
+  label VARCHAR NOT NULL
+);
+
+INSERT INTO locid.auth_node_label
+SELECT DISTINCT subject_id, lit_value
 FROM locid.auth_triple
-JOIN locid.nodes pn ON (pred_id = node_id)
+JOIN locid.node_aliases pa ON (pred_id = pa.node_id)
 JOIN locid.literals ON (object_id = lit_id)
-WHERE node_iri = 'http://www.w3.org/2000/01/rdf-schema#label';
+WHERE node_alias IN ('label', 'auth-label');
 
 CREATE INDEX IF NOT EXISTS auth_node_label_subj_idx
 ON locid.auth_node_label (subject_id);
 CREATE INDEX IF NOT EXISTS auth_node_label_lbl_idx
 ON locid.auth_node_label (label);
 ANALYZE locid.auth_node_label;
+
+--- #step Summarize gender data
+CREATE MATERIALIZED VIEW locid.mads_gender_summary AS
+SELECT COUNT(t.subject_id), o.node_id, o.node_iri, l.label
+FROM locid.auth_triple t
+JOIN locid.node_aliases pa ON (pred_id = pa.node_id)
+JOIN locid.nodes o ON (object_id = o.node_id)
+LEFT JOIN locid.auth_node_label l ON (object_id = l.subject_id)
+WHERE pa.node_alias = 'gender'
+GROUP BY o.node_id, o.node_iri, l.label
+ORDER BY COUNT(t.subject_id) DESC;
+
+CREATE MATERIALIZED VIEW locid.skos_gender AS
+SELECT nt.object_id AS node_id, l.label
+FROM
+  -- author triple
+  locid.auth_triple nt
+  -- author object to exclude
+  LEFT JOIN locid.nodes ton ON (nt.object_id = ton.node_id)
+  --- node type triples
+  JOIN locid.auth_triple tt ON (nt.object_id = tt.subject_id)
+  --- node labels
+  JOIN locid.auth_node_label l ON (l.subject_id = nt.object_id)
+WHERE
+  nt.pred_id = locid.common_node('gender')
+  AND ton.node_id IS NULL
+  AND tt.pred_id = locid.common_node('type')
+  AND tt.object_id = locid.common_node('concept');
--- loc-id-triple-index.sql
+++ loc-id-triple-index.sql
@@ -18,3 +18,36 @@ CREATE INDEX IF NOT EXISTS work_subject_idx ON locid.work_triple (subject_id);
 CREATE INDEX IF NOT EXISTS work_object_idx ON locid.work_triple (object_id);
 CLUSTER locid.work_triple USING work_subject_idx;
 ANALYZE locid.work_triple;
+
+--- #step Index well-known nodes
+CREATE TABLE IF NOT EXISTS locid.node_aliases (
+  node_alias VARCHAR UNIQUE NOT NULL,
+  node_id UUID UNIQUE NOT NULL,
+  node_iri VARCHAR UNIQUE NOT NULL
+);
+
+CREATE OR REPLACE PROCEDURE locid.alias_node (alias VARCHAR, iri VARCHAR)
+LANGUAGE plpgsql
+AS $ln$
+BEGIN
+    INSERT INTO locid.node_aliases (node_alias, node_id, node_iri)
+    SELECT alias, node_id, node_iri
+    FROM locid.nodes
+    WHERE node_iri = iri
+    ON CONFLICT DO NOTHING;
+END;
+$ln$;
+
+CREATE OR REPLACE FUNCTION locid.common_node(alias VARCHAR) RETURNS UUID
+  LANGUAGE SQL STABLE
+  AS $$
+  SELECT node_id FROM locid.node_aliases WHERE node_alias = alias;
+  $$;
+
+CALL locid.alias_node('label', 'http://www.w3.org/2000/01/rdf-schema#label');
+CALL locid.alias_node('auth-label', 'http://www.loc.gov/mads/rdf/v1#authoritativeLabel');
+CALL locid.alias_node('gender', 'http://www.loc.gov/mads/rdf/v1#gender');
+CALL locid.alias_node('concept', 'http://www.w3.org/2004/02/skos/core#Concept');
+CALL locid.alias_node('type', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type');
+ANALYSE locid.node_aliases;
+
