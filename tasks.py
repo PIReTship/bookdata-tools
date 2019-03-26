93deb8fd19624a7f83c79331880f4272a7072e17
Michael Ekstrand <michaelekstrand@boisestate.edu>
2019-03-26 10:40:22 -0600

    clean up status code

--- tasks.py
+++ tasks.py
@@ -23,17 +23,17 @@ except ImportError:
 @task
 def status(c):
     steps = support.get_steps(ns)
-    finished = set()
+    recorded = set()
     with support.database(autocommit=True) as db, db.cursor() as cur:
         cur.execute('SELECT step, started_at, finished_at, finished_at - started_at AS elapsed FROM import_status ORDER BY started_at')
         for step, start, end, time in cur:
-            finished.add(step)
+            recorded.add(step)
             if end:
                 print(f'{S.BRIGHT}{step}{S.RESET_ALL}: {F.GREEN}finished{S.RESET_ALL} at {end} (took {time})')
             else:
                 print(f'{S.BRIGHT}{step}{S.RESET_ALL}: {F.YELLOW}started{S.RESET_ALL} at {start}')
 
-    for step in sorted(k for k in steps.keys() if k not in finished):
+    for step in sorted(k for k in steps.keys() if k not in recorded):
         task = steps[step]
         print(f'{S.BRIGHT}{step}{S.RESET_ALL}: {F.RED}not run{S.RESET_ALL} (defined in {task})')
 
