83c315d398513cf0f43c05e20c2c67509ba20dec
Michael Ekstrand <md@ekstrandom.net>
2019-04-13 13:15:14 -0600

    make pcat a subcommand

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod cleaning;
 mod tsv;
 mod db;
 mod logging;
+mod commands;
 
 use structopt::StructOpt;
 
@@ -13,17 +14,30 @@ use indicatif::{ProgressBar, ProgressStyle};
 
 use error::Result;
 use logging::LogOpts;
+use commands::pcat;
 
 /// BookData import tools
 #[derive(StructOpt, Debug)]
 #[structopt(name="bookdata")]
 struct Opt {
   #[structopt(flatten)]
-  logging: LogOpts
+  logging: LogOpts,
+
+  #[structopt(subcommand)]
+  command: Command
+}
+
+#[derive(StructOpt, Debug)]
+#[allow(non_camel_case_types)]
+enum Command {
+  /// Output a file with a progress bar
+  pcat(pcat::Options)
 }
 
 fn main() -> Result<()> {
   let opt = Opt::from_args();
   opt.logging.init()?;
-  Ok(())
+  match opt.command {
+    Command::pcat(opts) => pcat::exec(opts)
+  }
 }
