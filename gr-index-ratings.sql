d55dc5baae18877c33bbc0075ab664764fceb6e5
Michael Ekstrand <michaelekstrand@boisestate.edu>
2019-06-20 16:37:34 -0600

    add work-interaction

--- gr-index-ratings.sql
+++ gr-index-ratings.sql
@@ -23,21 +23,26 @@ ANALYZE gr.user_info;
 
 --- #step Extract interaction data
 CREATE TABLE IF NOT EXISTS gr.interaction
-  AS SELECT gr_interaction_rid, book_id, gr_user_rid, rating, (gr_interaction_data->'isRead')::boolean AS is_read, date_added, date_updated
+  AS SELECT gr_interaction_rid, book_id AS gr_book_id, gr_work_id, gr_user_rid, rating, (gr_interaction_data->'isRead')::boolean AS is_read, date_added, date_updated
      FROM gr.raw_interaction,
           jsonb_to_record(gr_interaction_data) AS
               x(book_id INTEGER, user_id VARCHAR, rating INTEGER,
                 date_added TIMESTAMP WITH TIME ZONE, date_updated TIMESTAMP WITH TIME ZONE),
           gr.user_info
      WHERE user_id = gr_user_id;
+DROP VIEW IF EXISTS gr.work_interaction;
+CREATE VIEW gr.work_interaction
+AS SELECT ix.*, bid.gr_work_id
+FROM gr.interaction ix
+LEFT JOIN gr.book_ids bid USING (gr_book_id);
 
 --- #step Index interaction data
 --- #allow invalid_table_definition
 -- if this step is already done, first ALTER TABLE will fail
 ALTER TABLE gr.interaction ADD CONSTRAINT gr_interaction_pk PRIMARY KEY (gr_interaction_rid);
-CREATE INDEX IF NOT EXISTS interaction_book_idx ON gr.interaction (book_id);
+CREATE INDEX IF NOT EXISTS interaction_book_idx ON gr.interaction (gr_book_id);
 CREATE INDEX IF NOT EXISTS interaction_user_idx ON gr.interaction (gr_user_rid);
-ALTER TABLE gr.interaction ADD CONSTRAINT gr_interaction_book_fk FOREIGN KEY (book_id) REFERENCES gr_book_ids (gr_book_id);
+ALTER TABLE gr.interaction ADD CONSTRAINT gr_interaction_book_fk FOREIGN KEY (gr_book_id) REFERENCES gr_book_ids (gr_book_id);
 ALTER TABLE gr.interaction ADD CONSTRAINT gr_interaction_user_fk FOREIGN KEY (gr_user_rid) REFERENCES gr.user_info (gr_user_rid);
 ANALYZE gr.interaction;
 
@@ -48,7 +53,7 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS gr.rating
             (array_agg(rating ORDER BY date_updated DESC))[1] AS last_rating,
             COUNT(rating) AS nratings
      FROM gr.interaction
-            JOIN gr.book_cluster ON (gr_book_id = book_id)
+            JOIN gr.book_cluster USING (gr_book_id)
      WHERE rating > 0
      GROUP BY gr_user_rid, cluster;
 CREATE INDEX IF NOT EXISTS rating_user_idx ON gr.rating (user_id);
