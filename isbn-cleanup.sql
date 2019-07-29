e7bf3f4d46056a5b40faf98a58738e1bb089ca4d
Michael Ekstrand <md@ekstrandom.net>
2019-07-29 13:37:29 -0600

    isbn query

--- isbn-cleanup.sql
+++ isbn-cleanup.sql
@@ -4,3 +4,10 @@ AS $$
 SELECT upper(regexp_replace(substring(raw_isbn from
     '^\s*(?:(?:(?:ISBN)?[:;a-zA-Z]+?|\([[:digit:]]+\))\s*)?([0-9 -]+[0-9Xx])'), '[- ]', ''))
 $$;
+
+
+-- Debug query
+SELECT raw_isbn
+FROM locid.instance_isbn
+WHERE extract_isbn(raw_isbn) IS NULL
+LIMIT 50;
