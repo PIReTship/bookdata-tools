a511fcdeb59d49095b3b90af5ed74cd73eeec999
Michael Ekstrand <md@ekstrandom.net>
2020-03-31 19:42:21 -0600

    Commit book info updates

--- index/loc-mds-index-books.sql
+++ index/loc-mds-index-books.sql
@@ -100,24 +100,3 @@ REFRESH MATERIALIZED VIEW locmds.book_rec_isbn;
 CREATE INDEX IF NOT EXISTS book_rec_isbn_rec_idx ON locmds.book_rec_isbn (rec_id);
 CREATE INDEX IF NOT EXISTS book_rec_isbn_isbn_idx ON locmds.book_rec_isbn (isbn_id);
 ANALYZE locmds.book_rec_isbn;
-
---- #step Extract authors
-CREATE MATERIALIZED VIEW IF NOT EXISTS locmds.book_author_name
-  AS SELECT rec_id, regexp_replace(contents, '\W+$', '') AS name
-  FROM locmds.book_marc_field
-  WHERE tag = '100' AND sf_code = 'a'
-  WITH NO DATA;
-REFRESH MATERIALIZED VIEW locmds.book_author_name;
-CREATE INDEX IF NOT EXISTS book_author_name_rec_idx ON locmds.book_author_name (rec_id);
-CREATE INDEX IF NOT EXISTS book_author_name_name_idx ON locmds.book_author_name (name);
-ANALYZE locmds.book_author_name;
-
---= #step Extract publication years
-CREATE MATERIALIZED VIEW IF NOT EXISTS locmds.book_pub_year
-  AS SELECT rec_id, substring(contents from '(\d\d\d\d)') AS pub_year
-  FROM locmds.book_marc_field
-  WHERE tag = '260' AND sf_code = 'c' AND substring(contents from '(\d\d\d\d)') IS NOT NULL
-  WITH NO DATA;
-REFRESH MATERIALIZED VIEW locmds.book_pub_year;
-CREATE INDEX IF NOT EXISTS book_pub_year_rec_idx ON locmds.book_pub_year (rec_id);
-ANALYZE locmds.book_pub_year;
