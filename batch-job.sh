4c5e4723e50e6a4939c6cb2dfb6d8f975587bb3a
Michael Ekstrand <michaelekstrand@boisestate.edu>
2021-04-28 17:35:02 -0600

    SLURM support

---
+++ batch-job.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -J lkpy
+#SRUN -J lkpy
+
+node=$(hostname)
+echo "Running job on node $node" >&2
+
+# Boise State's SLURM cluster has aggresive ulimits, even with larger job requests
+# Reset those limits
+ulimit -v unlimited
+ulimit -u 2048
+ulimit -n 4096
+
+# Finally run the code
+exec "$@"
