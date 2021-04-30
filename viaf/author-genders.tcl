525da62a422cb4dea143fd986dc1c9572295fafd
Michael Ekstrand <michaelekstrand@boisestate.edu>
2021-04-30 15:45:07 -0600

    extract VIAF data

--- viaf/author-genders.tcl
+++ viaf/author-genders.tcl
@@ -1,8 +1,7 @@
 table fields "viaf.parquet"
 
-save-results "author-names.csv.gz" {
+save-results "author-genders.csv.gz" {
     SELECT rec_id, LOWER(TRIM(contents)) AS gender
     FROM fields
     WHERE tag = 375 AND sf_code = ASCII('a')
 }
-
