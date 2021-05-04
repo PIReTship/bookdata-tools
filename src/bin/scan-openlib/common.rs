98805a1e46bd3a86127d64da15b1e95d44f8537b
Michael Ekstrand <michaelekstrand@boisestate.edu>
2021-05-04 17:50:58 -0600

    nope

--- src/bin/scan-openlib/common.rs
+++ src/bin/scan-openlib/common.rs
@@ -1,7 +1,9 @@
 use std::str::FromStr;
+use std::fmt;
 
 use serde::Deserialize;
-use serde::de::DeserializeOwned;
+use serde::Deserializer;
+use serde::de;
 use thiserror::Error;
 
 use bookdata::prelude::*;
@@ -21,7 +23,7 @@ pub enum RowError {
   ParseError(#[from] serde_json::Error)
 }
 
-impl <T: DeserializeOwned> FromStr for Row<T> {
+impl <T: de::DeserializeOwned> FromStr for Row<T> {
   type Err = RowError;
 
   fn from_str(s: &str) -> Result<Row<T>, RowError> {
@@ -58,8 +60,12 @@ pub trait OLProcessor<T> where Self: Sized {
 
 /// Struct representing an author link in OL.
 #[derive(Deserialize, Debug)]
-pub struct Author {
-  pub author: Option<Keyed>
+pub enum Author {
+  Key(String),
+  Object {
+    author: Keyed
+  },
+  Empty
 }
 
 /// Keyed object reference
@@ -68,12 +74,58 @@ pub struct Keyed {
   pub key: String
 }
 
-
 impl Author {
   pub fn key<'a>(&'a self) -> Option<&'a str> {
     match self.author {
       Some(ref au) => Some(au.key.as_ref()),
       None => None
     }
+  }
+}
+
+
+// we have to manually implement Deserialize to handle truncated authors
+struct AuthorVisitor;
+
+impl <'de> Deserialize<'de> for Author {
+  fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+    deserializer.deserialize_map(AuthorVisitor)
+  }
+}
+
+impl <'de> de::Visitor<'de> for AuthorVisitor {
+  type Value = Author;
+
+  fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+    formatter.write_str("a string or object")
+  }
+
+  fn visit_str<E>(self, s: &str) -> Result<Self::Value, E> {
+    let key = match &s[0..3] {
+      "/a/" => "/author/".to_owned() + &s[3..],
+      _ => s.to_owned()
+    };
+    Ok({Author {
+      author: Some(Keyed {
+        key
+      })
+    }})
+  }
+
+  fn visit_map<A: de::MapAccess<'de>>(self, map: A) -> Result<Self::Value, A::Error> {
+    let mut map = map;
+    while let Some(e) = map.next_entry()? {
+      let (k, v): (&str, Keyed) = e;
+      if k == "author" {
+        return Ok(Author {
+          author: Some(v)
+        })
+      }
+    }
+
+    // no author found
+    Ok(Author {
+      author: None
+    })
   }
 }
