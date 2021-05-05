90e29f4de9c9b2c66a9d8820bce52dfe906bd9ed
Michael Ekstrand <michaelekstrand@boisestate.edu>
2021-05-05 15:23:13 -0600

    update isbn runs

--- scripts/collect-isbns.py
+++ scripts/collect-isbns.py
@@ -19,37 +19,38 @@ def read_column(file, column='isbn', format='parquet'):
         isbns = pd.read_csv(file)
     else:
         raise ValueError('invalid Parquet file')
-    return np.unique(isbns[column].dropna())
+    return isbns[column].dropna().value_counts()
 
 
 def read_goodreads(file):
     books = pd.read_parquet(file, columns=['isbn', 'isbn13', 'asin'])
-    isbns = np.concatenate([
-        books['isbn'].dropna().unique(),
-        books['isbn13'].dropna().unique(),
-        books['asin'].dropna().unique()
-    ])
-    return isbns
+    i10 = books['isbn'].dropna().value_counts()
+    i13 = books['isbn13'].dropna().value_counts()
+    asin = books['asin'].dropna().value_counts()
+    all = pd.concat([i10, i13, asin])
+    if not all.index.is_unique():
+        _log.warn('duplicate GR ISBNs')
+        all = all.groupby(all.index).sum()
+    return all
 
 
 _log.info('reading LOC ISBNs')
-loc_isbns = read_column('loc-mds/book-isbns.parquet')
+loc_isbns = read_column('loc-mds/book-isbns.parquet').to_frame(name='LOC')
 _log.info('reading OL ISBNs')
-ol_isbns = read_column('openlibrary/edition-isbns.parquet')
+ol_isbns = read_column('openlibrary/edition-isbns.parquet').to_frame('OL')
 _log.info('reading GR ISBNs')
-gr_isbns = read_goodreads('goodreads/gr-book-ids.parquet')
+gr_isbns = read_goodreads('goodreads/gr-book-ids.parquet').to_frame('GR')
 _log.info('reading BX ISBNs')
-bx_isbns = read_column('data/bx-ratings.csv', format='csv')
+bx_isbns = read_column('data/bx-ratings.csv', format='csv').to_frame('BX')
 
 _log.info('combining ISBN lists')
-all_isbns = np.unique(np.concatenate([
-    loc_isbns, ol_isbns, gr_isbns, bx_isbns
-]))
-_log.info('found %d unique ISBNs', len(all_isbns))
+isbns = loc_isbns.join(ol_isbns, how='outer')
+isbns = isbns.join(gr_isbns, how='outer')
+isbns = isbns.join(bx_isbns, how='outer')
+isbns.sort_index(inplace=True)
+_log.info('found %d unique ISBNs', len(isbns))
+isbns['isbn_id'] = np.arange(len(isbns), dtype=np.int32) + 1
+isbns = isbns.reset_index()
 
 _log.info('writing output file')
-df = pd.DataFrame({
-    'isbn_id': np.arange(len(all_isbns), dtype=np.int32),
-    'isbn': all_isbns
-})
-df.to_parquet('book-links/all-isbns.parquet', compression='zstd', index=False)
+isbns.to_parquet('book-links/all-isbns.parquet', compression='zstd', index=False)
