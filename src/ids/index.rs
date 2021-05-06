84083a4ca286a7572dfa2a75aef37c11a8f44a83
Michael Ekstrand <md@ekstrandom.net>
2021-05-06 09:41:19 -0600

    test index save/load

--- src/ids/index.rs
+++ src/ids/index.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::path::{Path};
 use std::fs::File;
 use std::collections::HashMap;
 use std::hash::Hash;
@@ -13,6 +13,11 @@ use parquet::record::reader::RowIter;
 use parquet::record::RowAccessor;
 use crate::io::ObjectWriter;
 use crate::arrow::*;
+
+#[cfg(test)]
+use quickcheck::{Arbitrary,Gen};
+#[cfg(test)]
+use tempfile::tempdir;
 
 use crate as bookdata;
 
@@ -49,7 +54,7 @@ impl <K> IdIndex<K> where K: Eq + Hash {
   }
 
   /// Look up the ID for a key if it is present.
-  pub fn lookup<Q>(&mut self, key: &Q) -> Option<u32> where K: Borrow<Q>, Q: Hash + Eq + ?Sized {
+  pub fn lookup<Q>(&self, key: &Q) -> Option<u32> where K: Borrow<Q>, Q: Hash + Eq + ?Sized {
     self.map.get(key).map(|i| *i)
   }
 }
@@ -99,21 +104,21 @@ impl IdIndex<String> {
   }
 
   /// Save to a Parquet file with the standard configuration.
-  pub fn save_standard<P: AsRef<Path>>(self, path: P) -> Result<()> {
+  pub fn save_standard<P: AsRef<Path>>(&self, path: P) -> Result<()> {
     self.save(path, "id", "key")
   }
 
   /// Save to a Parquet file with the standard configuration.
-  pub fn save<P: AsRef<Path>>(self, path: P, id_col: &str, key_col: &str) -> Result<()> {
+  pub fn save<P: AsRef<Path>>(&self, path: P, id_col: &str, key_col: &str) -> Result<()> {
     let mut wb = TableWriterBuilder::new();
     wb.rename("id", id_col);
     wb.rename("key", key_col);
     let mut writer = wb.open(path)?;
 
-    for (k, v) in self.map {
+    for (k, v) in &self.map {
       writer.write_object(IdRec {
-        id: v,
-        key: k
+        id: *v,
+        key: k.clone()
       })?;
     }
 
@@ -121,4 +126,75 @@ impl IdIndex<String> {
 
     Ok(())
   }
+}
+
+
+#[test]
+fn test_index_empty() {
+  let index: IdIndex<String> = IdIndex::new();
+  assert_eq!(index.len(), 0);
+  assert!(index.lookup("bob").is_none());
+}
+
+
+#[test]
+fn test_index_intern_one() {
+  let mut index: IdIndex<String> = IdIndex::new();
+  assert!(index.lookup("hackem muche").is_none());
+  let id = index.intern("hackem muche".to_string());
+  assert_eq!(id, 1);
+  assert_eq!(index.lookup("hackem muche").unwrap(), 1);
+}
+
+
+#[test]
+fn test_index_intern_two() {
+  let mut index: IdIndex<String> = IdIndex::new();
+  assert!(index.lookup("hackem muche").is_none());
+  let id = index.intern("hackem muche".to_string());
+  assert_eq!(id, 1);
+  let id2 = index.intern("readme".to_string());
+  assert_eq!(id2, 2);
+  assert_eq!(index.lookup("hackem muche").unwrap(), 1);
+}
+
+
+#[test]
+fn test_index_intern_twice() {
+  let mut index: IdIndex<String> = IdIndex::new();
+  assert!(index.lookup("hackem muche").is_none());
+  let id = index.intern("hackem muche".to_string());
+  assert_eq!(id, 1);
+  let id2 = index.intern("hackem muche".to_string());
+  assert_eq!(id2, 1);
+  assert_eq!(index.len(), 1);
+}
+
+#[test]
+fn test_index_save() -> Result<()> {
+  let mut index: IdIndex<String> = IdIndex::new();
+  let mut gen = Gen::new(100);
+  for i in 0..10000 {
+    let key = String::arbitrary(&mut gen);
+    let prev = index.lookup(&key);
+    let id = index.intern(key);
+    match prev {
+      Some(i) => assert_eq!(id, i),
+      None => assert_eq!(id as usize, index.len())
+    };
+  }
+
+  let dir = tempdir()?;
+  let pq = dir.path().join("index.parquet");
+  index.save_standard(&pq).expect("save error");
+
+  let i2 = IdIndex::load_standard(&pq).expect("load error");
+  assert_eq!(i2.len(), index.len());
+  for (k, v) in &index.map {
+    let v2 = i2.lookup(k);
+    assert!(v2.is_some());
+    assert_eq!(v2.unwrap(), *v);
+  }
+
+  Ok(())
 }
