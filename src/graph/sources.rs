c13af11d3900b10de698e7b13132abc17aab7d12
Michael Ekstrand <md@ekstrandom.net>
2021-05-05 22:07:14 -0600

    fix bad file

--- src/graph/sources.rs
+++ src/graph/sources.rs
@@ -247,7 +247,7 @@ impl <Q: QueryContext> EdgeQuery<Q> for GRBooks {
 
 impl NodeRead for GRWorks {
   fn read_node_ids(&self, ctx: &mut ExecutionContext) -> Result<Arc<dyn DataFrame>> {
-    let df = ctx.read_parquet("goodreads/gr-work-ids.parquet")?;
+    let df = ctx.read_parquet("goodreads/gr-book-ids.parquet")?;
     let df = df.filter(col("work_id").is_not_null())?;
     let df = df.select(vec![
       col("work_id") + lit(NS_GR_WORK.base())
