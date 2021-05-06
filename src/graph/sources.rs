57826899344a8e9b37e5bc47c001d7463e971eed
Michael Ekstrand <md@ekstrandom.net>
2021-05-05 22:29:53 -0600

    log schemas

--- src/graph/sources.rs
+++ src/graph/sources.rs
@@ -82,6 +82,7 @@ impl NodeRead for ISBN {
     let df = df.select(vec![
       (col("isbn_id") + lit(NS_ISBN.base())).alias("id")
     ])?;
+    info!("ISBN schema: {:?}", df.schema());
     Ok(df)
   }
 }
@@ -102,6 +103,7 @@ impl NodeRead for LOC {
     let df = df.select(vec![
       (col("rec_id") + lit(NS_LOC_REC.base())).alias("id")
     ])?;
+    info!("LOC book schema: {:?}", df.schema());
     Ok(df)
   }
 }
@@ -138,6 +140,7 @@ impl NodeRead for OLEditions {
     let df = df.select(vec![
       (col("id") + lit(NS_EDITION.base())).alias("id")
     ])?;
+    info!("OL edition schema: {:?}", df.schema());
     Ok(df)
   }
 }
@@ -176,6 +179,7 @@ impl NodeRead for OLWorks {
     let df = df.select(vec![
       (col("id") + lit(NS_WORK.base())).alias("id")
     ])?;
+    info!("OL work schema: {:?}", df.schema());
     Ok(df)
   }
 }
@@ -216,6 +220,7 @@ impl NodeRead for GRBooks {
     let df = df.select(vec![
       (col("book_id") + lit(NS_GR_BOOK.base())).alias("id")
     ])?;
+    info!("GR book schema: {:?}", df.schema());
     Ok(df)
   }
 }
@@ -252,6 +257,7 @@ impl NodeRead for GRWorks {
     let df = df.select(vec![
       (col("work_id") + lit(NS_GR_WORK.base())).alias("id")
     ])?;
+    info!("GR work schema: {:?}", df.schema());
     Ok(df)
   }
 }
