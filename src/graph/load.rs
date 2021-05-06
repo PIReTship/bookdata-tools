6fb113d801d6862e076ea37c3cd4e0a49022ee87
Michael Ekstrand <michaelekstrand@boisestate.edu>
2021-05-06 14:00:45 -0600

    Include all known nodes

--- src/graph/load.rs
+++ src/graph/load.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap};
 
 use log::*;
 use anyhow::Result;
@@ -13,29 +13,22 @@ use super::{IdGraph,IdNode};
 
 type NodeMap = HashMap<i32, IdNode>;
 
-#[allow(dead_code)]
-async fn add_vertices(g: &mut IdGraph, ctx: &mut ExecutionContext, src: &dyn NodeRead) -> Result<()> {
+async fn add_vertices(g: &mut IdGraph, nodes: &mut NodeMap, ctx: &mut ExecutionContext, src: &dyn NodeRead) -> Result<()> {
   info!("scanning vertices from {:?}", src);
   let node_df = src.read_node_ids(ctx)?;
   let batches = node_df.collect().await?;
-
-  let mut node_ids = HashSet::new();
+  let ninit = nodes.len();
 
   for batch in batches {
-    let col = batch.column(0);
-    let col = col.as_any().downcast_ref::<Int32Array>().expect("invalid column type");
-    for id in col.iter() {
-      if let Some(id) = id {
-        node_ids.insert(id);
+    let col = batch.column(0).as_any().downcast_ref::<Int32Array>().unwrap();
+    for id in col.iter().flatten() {
+      if !nodes.contains_key(&id) {
+        nodes.insert(id, g.add_node(id));
       }
     }
   }
 
-  info!("loaded {} vertices from {:?}", node_ids.len(), src);
-
-  for id in node_ids {
-    g.add_node(id);
-  }
+  info!("loaded {} new vertices from {:?}", nodes.len() - ninit, src);
 
   Ok(())
 }
@@ -68,12 +61,11 @@ async fn add_edges(g: &mut IdGraph, nodes: &mut NodeMap, ctx: &mut ExecutionCont
 pub async fn load_graph() -> Result<IdGraph> {
   let mut graph = IdGraph::new_undirected();
   let mut ctx = ExecutionContext::new();
-  // info!("loading nodes");
-  // for src in node_sources() {
-  //   add_vertices(&mut graph, &mut ctx, src.as_ref()).await?;
-  // }
-  // info!("indexing nodes");
   let mut nodes = NodeMap::new();
+  info!("loading nodes");
+  for src in node_sources() {
+    add_vertices(&mut graph, &mut nodes, &mut ctx, src.as_ref()).await?;
+  }
   for src in edge_sources() {
     add_edges(&mut graph, &mut nodes, &mut ctx, src.as_ref()).await?;
   }
