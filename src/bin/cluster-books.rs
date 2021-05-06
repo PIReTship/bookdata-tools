c94489df3d65657729b32a540b49f0fde8a3cadd
Michael Ekstrand <md@ekstrandom.net>
2021-05-05 23:23:11 -0600

    add cluster-books command

--- src/bin/cluster-books.rs
+++ src/bin/cluster-books.rs
@@ -1,22 +1,11 @@
-use std::path::{PathBuf};
-use std::time::Instant;
-use std::fs::{File, read_to_string};
-use std::future::Future;
-use std::sync::Arc;
-
 use tokio;
-use tokio::runtime::Runtime;
 
 use bookdata::prelude::*;
-use bookdata::arrow::fusion::*;
+use bookdata::arrow::*;
 use bookdata::graph::load_graph;
+use bookdata::ids::codes::NS_ISBN;
 
-use flate2::write::GzEncoder;
-use molt::*;
-use parquet::basic::Compression;
-use parquet::file::properties::WriterProperties;
-use datafusion::execution::context::ExecutionContext;
-use datafusion::physical_plan::{ExecutionPlan};
+use petgraph::algo::tarjan_scc;
 
 /// Run the book clustering algorithm.
 #[derive(StructOpt, Debug)]
@@ -26,12 +15,42 @@ pub struct ClusterBooks {
   common: CommonOpts,
 }
 
+#[derive(TableRow)]
+struct ClusterRec {
+  isbn_id: i32,
+  cluster: i32
+}
+
 #[tokio::main]
 pub async fn main() -> Result<()> {
   let opts = ClusterBooks::from_args();
   opts.common.init()?;
 
   let graph = load_graph().await?;
+
+  info!("computing connected components");
+  let clusters = tarjan_scc(&graph);
+
+  info!("writing clusters");
+  let mut writer = TableWriter::open("book-links/isbn-clusters.parquet")?;
+
+  for ci in 0..clusters.len() {
+    let verts = &clusters[ci];
+    let vids: Vec<_> = verts.iter().map(|v| {
+      *(graph.node_weight(*v).unwrap())
+    }).collect();
+    let code = vids.iter().min().unwrap();
+    for v in &vids {
+      if let Some(id) = NS_ISBN.from_code(*v) {
+        writer.write_object(ClusterRec {
+          isbn_id: id,
+          cluster: *code
+        })?;
+      }
+    }
+  }
+
+  writer.finish()?;
 
   Ok(())
 }
