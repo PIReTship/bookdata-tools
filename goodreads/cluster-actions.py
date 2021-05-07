0cb725ec41c2daa977e007319235fab5e1675484
Michael Ekstrand <michaelekstrand@boisestate.edu>
2021-05-07 14:40:52 -0600

    dup stuff

--- goodreads/cluster-actions.py
+++ goodreads/cluster-actions.py
@@ -5,13 +5,14 @@ Set things up for cluster-based ratings.
 import logging
 import sys
 
+import numpy as np
 import pandas as pd
 
 _log = logging.getLogger('cluster-ratings')
 logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 
 _log.info('reading actions')
-actions = pd.read_parquet('gr-interactions.parquet', columns=['user_id', 'cluster', 'rating', 'updated'])
+actions = pd.read_parquet('gr-interactions.parquet', columns=['user_id', 'cluster', 'updated'])
 actions.info()
 
 _log.info('filtering actions')
@@ -19,9 +20,20 @@ actions = actions[actions['cluster'].notnull()].astype({'cluster': 'i4'})
 actions.reset_index(inplace=True, drop=True)
 actions.info()
 
-_log.info('summarizing actions')
-last_act = actions.groupby(['user_id', 'cluster'])['updated'].agg(['max', 'min', 'count'])
-last_act = last_act.reset_index(['user_id', 'cluster'])
+_log.info('finding unique actions')
+dups = actions.duplicated(['user_id', 'cluster'], False)
+singles = actions[~dups].reset_index(drop=True).rename(columns={
+    'user_id': 'user',
+    'cluster': 'item',
+    'updated': 'timestamp'
+})
+singles['first_time'] = singles['timestamp']
+singles['nactions'] = 1
+
+_log.info('summarizing non-unique actions')
+duped = actions[dups].reset_index(drop=True)
+last_act = duped.groupby(['user_id', 'cluster'])['updated'].agg(['max', 'min', 'count'])
+last_act = last_act.reset_index()
 last_act.info()
 last_act = last_act.rename(columns={
     'user_id': 'user',
@@ -29,12 +41,17 @@ last_act = last_act.rename(columns={
     'max': 'timestamp',
     'min': 'first_time',
     'count': 'nactions'
-}).astype({
+})
+last_act.info()
+
+_log.info('combining actions')
+result = pd.concat([singles, last_act], ignore_index=True)
+result = result.astype({
     'user': 'i4',
     'item': 'i4',
     'nactions': 'i4'
 })
-last_act.info()
+result.info()
 
 _log.info('writing actions')
-last_act.to_parquet('gr-cluster-actions.parquet', compression='zstd', index=False)
+result.to_parquet('gr-cluster-actions.parquet', compression='zstd', index=False)
