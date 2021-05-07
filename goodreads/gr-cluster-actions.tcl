06bc402b8f5fbeada3e5f620e6a8b22b8c78aa3b
Michael Ekstrand <michaelekstrand@boisestate.edu>
2021-05-06 19:07:13 -0600

    delete unused tcls

--- goodreads/gr-cluster-actions.tcl
+++
@@ -1,9 +0,0 @@
-table actions "gr-interactions.parquet"
-table clusters "gr-book-link.parquet"
-
-save-results "gr-cluster-actions.parquet" {
-    SELECT user_id AS user, cluster AS item, COUNT(user_id) AS nactions
-    FROM actions
-    JOIN clusters USING (book_id)
-    GROUP BY user_id, cluster
-}
