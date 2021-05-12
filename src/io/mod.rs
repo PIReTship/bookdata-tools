e7bea269677caf1476a4dc8b5645742e20aa2531
Michael Ekstrand <michaelekstrand@boisestate.edu>
2021-05-12 17:37:16 -0600

    remove hash stuff

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -1,11 +1,9 @@
 pub mod progress;
-pub mod hash;
 pub mod delim;
 pub mod compress;
 pub mod lines;
 pub mod object;
 
-pub use hash::{HashRead, HashWrite};
 pub use delim::DelimPrinter;
 pub use lines::LineProcessor;
 pub use compress::open_gzin_progress;
