f6c3ebc36194fd387bda15cbd5cdac2e368fcc7f
Michael Ekstrand <michaelekstrand@boisestate.edu>
2021-05-19 17:29:27 -0600

    Emit indicator codes

--- viaf/author-names.tcl
+++ viaf/author-names.tcl
@@ -1,7 +1,7 @@
 table fields "viaf.parquet"
 
 save-results "author-names.csv.gz" {
-    SELECT rec_id, ind1, TRIM(contents) AS name
+    SELECT rec_id, CHR(ind1) AS ind1, CHR(ind2) AS ind2, CHR(sf_code), TRIM(contents) AS name
     FROM fields
     WHERE tag = 700 AND sf_code = ASCII('a')
 }
