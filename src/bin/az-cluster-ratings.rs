44e939287a350f787b771766cc0c5ff88fb9b801
Michael Ekstrand <michaelekstrand@boisestate.edu>
2021-06-07 13:49:03 -0600

    try scanning

--- src/bin/az-cluster-ratings.rs
+++ src/bin/az-cluster-ratings.rs
@@ -7,7 +7,10 @@ use serde::{Deserialize};
 
 use tokio;
 
+use arrow::record_batch::RecordBatch;
+use arrow::util::pretty::print_batches;
 use datafusion::prelude::*;
+use datafusion::datasource::memory::MemTable;
 use bookdata::prelude::*;
 use bookdata::arrow::fusion::*;
 use bookdata::ratings::RatingDedup;
@@ -20,6 +23,14 @@ use anyhow::Result;
 pub struct ClusterRatings {
   #[structopt(flatten)]
   common: CommonOpts,
+
+  /// Show the logical plan for the data frame and exit.
+  #[structopt(long="logical-plan")]
+  logical_plan: bool,
+
+  /// Show the physical plan for the data frame and exit.
+  #[structopt(long="physical-plan")]
+  physical_plan: bool,
 
   /// Rating output file
   #[structopt(short="o", long="output", name="FILE", parse(from_os_str))]
@@ -68,10 +79,29 @@ async fn main() -> Result<()> {
   let ic_link = isbns.join(clusters, JoinType::Inner, &["isbn_id"], &["isbn_id"])?;
   let ic_link = ic_link.select_columns(&["isbn", "cluster"])?;
 
+  // force a scan of linking info
+  info!("scanning ISBN/cluster links");
+  let icbatch: Vec<Vec<RecordBatch>> = ic_link.collect_partitioned().await?;
+  let icprov = MemTable::try_new(ic_link.schema().clone().into(), icbatch)?;
+  let icprov = Arc::new(icprov);
+  let ic_link = ctx.read_table(icprov)?;
+
   // load ratings
   let ratings = ctx.read_parquet(opts.infile.as_str())?;
   let rlink = ratings.join(ic_link, JoinType::Left, &["asin"], &["isbn"])?;
   let rlink = rlink.select_columns(&["user", "cluster", "rating", "timestamp"])?;
+
+  if opts.logical_plan {
+    let plan = rlink.to_logical_plan();
+    let plan = ctx.optimize(&plan)?;
+    println!("Logical plan:\n{}", plan.display_indent());
+    return Ok(())
+  } else if opts.physical_plan {
+    let exp = rlink.explain(true)?;
+    let res: Vec<RecordBatch> = exp.collect().await?;
+    print_batches(&res)?;
+    return Ok(())
+  }
 
   let dedup = scan_dedup(&mut ctx, rlink).await?;
 
