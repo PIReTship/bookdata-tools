86fce6fb317af47927d82ae983b8c6d647147832
Michael Ekstrand <michaelekstrand@boisestate.edu>
2021-06-07 15:55:18 -0600

    add action scanning

--- src/ratings.rs
+++ src/ratings.rs
@@ -21,9 +21,34 @@ pub struct RatingRecord {
   nratings: u32,
 }
 
+/// Record for a single output action.
+#[derive(TableRow, Debug)]
+pub struct ActionRecord {
+  user: u32,
+  item: i32,
+  first_time: i64,
+  last_time: i64,
+  last_rating: Option<f32>,
+  nactions: u32,
+}
+
+#[derive(Debug, Hash, Eq, PartialEq)]
+struct Key {
+  user: u32,
+  item: i32
+}
+
+impl Key {
+  fn new(user: u32, item: i32) -> Key {
+    Key {
+      user, item
+    }
+  }
+}
+
 /// Rating deduplicator.
 pub struct RatingDedup {
-  table: HashMap<(u32, i32), Vec<(f32, i64)>>
+  table: HashMap<Key, Vec<(f32, i64)>>
 }
 
 impl RatingDedup {
@@ -36,7 +61,7 @@ impl RatingDedup {
 
   /// Add a rating to the deduplicator.
   pub fn record(&mut self, user: u32, item: i32, rating: f32, timestamp: i64) {
-    let k = (user, item);
+    let k = Key::new(user, item);
     // get the vector for this user/item pair
     let vec = self.table.entry(k).or_insert_with(|| Vec::with_capacity(1));
     // and insert our records!
@@ -51,12 +76,12 @@ impl RatingDedup {
     // we're going to consume the hashtable.
     for (k, vec) in self.table {
       let mut vec = vec;
-      let (user, item) = k;
       if vec.len() == 1 {
         // fast path
         let (rating, timestamp) = vec[0];
         writer.write_object(RatingRecord {
-          user, item,
+          user: k.user,
+          item: k.item,
           rating, timestamp,
           last_rating: rating,
           last_time: timestamp,
@@ -77,10 +102,87 @@ impl RatingDedup {
         let (last_rating, last_time) = vec[vec.len() - 1];
 
         writer.write_object(RatingRecord {
-          user, item,
+          user: k.user,
+          item: k.item,
           rating, timestamp,
           last_rating, last_time,
           nratings: vec.len() as u32
+        })?;
+      }
+    }
+
+    writer.finish()
+  }
+}
+
+
+#[derive(PartialEq, Clone, Debug)]
+struct Action {
+  timestamp: i64,
+  rating: Option<f32>,
+}
+
+/// Action deduplicator.
+pub struct ActionDedup {
+  table: HashMap<Key, Vec<Action>>
+}
+
+impl ActionDedup {
+  /// Create a new deduplicator.
+  pub fn new() -> ActionDedup {
+    ActionDedup {
+      table: HashMap::new()
+    }
+  }
+
+  /// Add an action to the deduplicator.
+  pub fn record(&mut self, user: u32, item: i32, timestamp: i64, rating: Option<f32>) {
+    let k = Key::new(user, item);
+    // get the vector for this user/item pair
+    let vec = self.table.entry(k).or_insert_with(|| Vec::with_capacity(1));
+    // and insert our records!
+    vec.push(Action {
+      timestamp, rating
+    });
+  }
+
+  /// Save the rating table disk.
+  pub fn write_actions<P: AsRef<Path>>(self, path: P) -> Result<usize> {
+    info!("writing {} deduplicated actions to {}", self.table.len(), path.as_ref().display());
+    let mut writer = TableWriter::open(path)?;
+
+    // we're going to consume the hashtable.
+    for (k, vec) in self.table {
+      let mut vec = vec;
+      if vec.len() == 1 {
+        // fast path
+        let act = &vec[0];
+        writer.write_object(ActionRecord {
+          user: k.user,
+          item: k.item,
+          first_time: act.timestamp,
+          last_time: act.timestamp,
+          last_rating: act.rating,
+          nactions: 1
+        })?;
+      } else {
+        vec.sort_unstable_by_key(|a| a.timestamp);
+        let first = &vec[0];
+        let last = &vec[vec.len() - 1];
+        let rates = vec.iter().flat_map(|a| a.rating).collect::<Vec<f32>>();
+        let last_rating = if rates.len() > 0 {
+          Some(rates[rates.len() - 1])
+        } else {
+          None
+        };
+
+        writer.write_object(ActionRecord {
+          user: k.user,
+          item: k.item,
+          first_time: first.timestamp,
+          last_time: last.timestamp,
+          last_rating,
+          nactions: vec.len() as u32
         })?;
       }
     }
