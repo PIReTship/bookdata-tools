d37c2070ff442e84e9a0d9a7c0bcb5ecf5a4cf46
Michael Ekstrand <michaelekstrand@boisestate.edu>
2021-06-08 10:36:35 -0600

    bump processes

--- batch-job.sh
+++ batch-job.sh
@@ -8,7 +8,7 @@ echo "Running job on node $node" >&2
 # Boise State's SLURM cluster has aggresive ulimits, even with larger job requests
 # Reset those limits
 ulimit -v unlimited
-ulimit -u 2048
+ulimit -u 4096
 ulimit -n 4096
 
 # Finally run the code
