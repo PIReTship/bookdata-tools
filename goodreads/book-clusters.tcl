a9f909937e9e82f3013192de3137c3d9d191b4eb
Michael Ekstrand <michaelekstrand@boisestate.edu>
2021-09-29 17:07:13 -0600

    try that

--- goodreads/book-clusters.tcl
+++ goodreads/book-clusters.tcl
@@ -2,9 +2,9 @@ table clusters "../book-links/cluster-codes.parquet"
 table book_ids "gr-book-ids.parquet"
 
 save-results "gr-book-link.parquet" {
-    SELECT book_ids.book_id, work_id, cluster
+    SELECT book_id, work_id, cluster
     FROM book_ids
-    JOIN (SELECT gr_book_from_code(book_code) AS book_id, cluster
+    JOIN (SELECT gr_book_from_code(book_code) AS grbid, cluster
           FROM clusters
-          WHERE code_is_gr_book(book_code)) bc USING (book_id)
+          WHERE code_is_gr_book(book_code)) bc ON book_id = grbid
 }
