7b80d8d2a74791be084a44e20304729bee72da1f
Michael Ekstrand <michaelekstrand@boisestate.edu>
2021-09-29 19:03:32 -0600

    I do not understand my statistics.

--- ClusterStats.py
+++ ClusterStats.py
@@ -107,3 +107,65 @@ with open('book-links/cluster-stats.json', 'w') as sf:
     json.dump({
         'clusters': len(clusters)
     }, sf)
+
+# %% [markdown]
+# ## Debugging GoodReads Clusters
+#
+# Let's find the clusters w/ GR but no ISBNs.
+
+# %%
+no_isbn = clusters['n_isbns'] < 1
+have_gr = clusters['n_gr_books'] > 0
+bad = clusters[no_isbn & have_gr]
+bad.info()
+
+# %% [markdown]
+# These are complete what-the-hell. Everything is supposed to have ISBNs.
+
+# %%
+gr_links = pd.read_parquet('goodreads/gr-book-link.parquet')
+gr_links.set_index('book_id', inplace=True)
+gr_links.head()
+
+# %%
+gr_works = pd.read_parquet('goodreads/gr-work-info.parquet', columns=['work_id', 'title', 'pub_year'])
+gr_works.set_index('work_id', inplace=True)
+gr_works.head()
+
+# %%
+gr_info = pd.read_parquet('goodreads/gr-book-ids.parquet')
+gr_info.set_index('book_id', inplace=True)
+gr_info.head()
+
+# %%
+bad_link = gr_links.join(bad, on='cluster', how='right')
+bad_link.head()
+
+# %%
+bad_li = bad_link.join(gr_info.drop(columns=['work_id']))
+bad_li
+
+# %% [markdown]
+# This shows that these are books with no ISBN data.
+
+# %% [markdown]
+# ### GR-only clusters
+
+# %% [markdown]
+# Now let's look for clusters that only have GoodReads records.
+
+# %%
+gr_only = clusters['n_loc_recs'] < 1
+gr_only &= clusters['n_ol_editions'] < 1
+gr_only &= clusters['n_ol_works'] < 1
+gr_cs = clusters[gr_only].sort_values('n_nodes', ascending=False)
+gr_cs
+
+# %% [markdown]
+# Let's look at IDs in one of these early clusters.
+
+# %%
+big = gr_links[gr_links['cluster'] == gr_cs.index[2]]
+big[['cluster']].join(gr_info).join(gr_works, on='work_id')
+
+# %%
