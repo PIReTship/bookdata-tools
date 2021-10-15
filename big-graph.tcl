21d69a3479cb4255048aa6cac307eca4d586eadb
Michael Ekstrand <michaelekstrand@boisestate.edu>
2021-10-14 21:59:28 -0600

    more debug

--- big-graph.tcl
+++ big-graph.tcl
@@ -1,23 +1,27 @@
 table editions "openlibrary/editions.parquet"
 table works "openlibrary/works.parquet"
 table edition_works "openlibrary/edition-works.parquet"
+table ed_isbn "openlibrary/edition-isbns.parquet"
 
-set id 18334306
+set id 9312043
 
 query {
     SELECT *
     FROM works
-    WHERE key = '/works/OL8193418W'
+    WHERE id = 9312043
 }
 
 query {
-    SELECT *
-    FROM editions
-    WHERE key = '/books/OL29216808M'
-}
-
-query {
-    SELECT *
+    SELECT COUNT(*)
     FROM edition_works
-    WHERE work = 18334306
+    WHERE work = 9312043
+}
+
+query {
+    SELECT isbn, COUNT(ew.edition)
+    FROM edition_works ew
+    JOIN ed_isbn ei ON ew.edition = ei.edition
+    WHERE ew.work = 9312043
+    GROUP BY isbn
+    HAVING COUNT(ew.edition) > 1
 }
