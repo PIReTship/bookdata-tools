ee701b89b68e1013a8d425cde0a7b3331a40901d
Michael Ekstrand <michaelekstrand@boisestate.edu>
2021-11-09 17:49:52 -0700

    isn isbns

--- goodreads/book-isbn-ids.tcl
+++ goodreads/book-isbn-ids.tcl
@@ -9,4 +9,8 @@ save-results "book-isbn-ids.parquet" {
     SELECT book_id, isbn_id
     FROM books JOIN isbns ON (isbn13 = isbn)
     WHERE isbn13 IS NOT NULL
+    UNION ALL
+    SELECT book_id, isbn_id
+    FROM books JOIN isbns ON (isbn13 = isbn)
+    WHERE asin IS NOT NULL
 }
