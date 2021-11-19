a50ce16691a40f50a0bbdb9c368bcbc3c5624f47
Michael Ekstrand <michaelekstrand@boisestate.edu>
2021-11-19 14:03:47 -0700

    RDF wrapper enum

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -4,8 +4,6 @@
 //! compilation time and disk space in common configurations, with different tools
 //! implemented as subcommands.  Each subcommand implements the [Command] trait, which
 //! exposes the command line arguments and invocation.
-// mod support;
-
 pub mod fusion;
 pub mod index_names;
 pub mod rdf_scan_nodes;
@@ -21,11 +19,33 @@ pub trait Command {
   fn exec(self) -> Result<()>;
 }
 
+/// Enum to collect and dispatch CLI commands.
 #[enum_dispatch(Command)]
 #[derive(StructOpt, Debug)]
 pub enum BDCommand {
   Fusion(fusion::Fusion),
   IndexNames(index_names::IndexNames),
+  RDF(RDFWrapper)
+}
+
+/// Tools for processing RDF.
+#[derive(StructOpt, Debug)]
+pub struct RDFWrapper {
+  #[structopt(subcommand)]
+  command: RDFCommand
+}
+
+impl Command for RDFWrapper {
+  fn exec(self) -> Result<()> {
+    self.command.exec()
+  }
+}
+
+#[enum_dispatch(Command)]
+#[derive(StructOpt, Debug)]
+pub enum RDFCommand {
+  ScanNodes(rdf_scan_nodes::ScanNodes),
+  ScanTriples(rdf_scan_triples::ScanTriples),
 }
 
 use happylog::args::LogOpts;
