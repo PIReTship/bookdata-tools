335bb75d8b5185b0d4a1c182f3df8dd6517b7448
Michael Ekstrand <michaelekstrand@boisestate.edu>
2021-12-04 15:03:59 -0700

    Remove progress module

--- src/prelude.rs
+++ src/prelude.rs
@@ -6,7 +6,6 @@ pub use log::*;
 pub use structopt::StructOpt;
 pub use fallible_iterator::FallibleIterator;
 pub use crate::io::LineProcessor;
-pub use crate::io::progress::default_progress;
 pub use crate::io::ObjectWriter;
 pub use crate::io::DataSink;
 pub use crate::io::path_list;
