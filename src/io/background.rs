5ea4c5144cb282c0561bc8919efacf7c4b56f7e9
Michael Ekstrand <michaelekstrand@boisestate.edu>
2022-06-03 13:08:18 -0600

    add background writer

--- src/io/background.rs
+++ src/io/background.rs
@@ -1,13 +1,21 @@
 use std::io::prelude::*;
 use std::io::{self, copy};
+use std::mem::drop;
 use std::thread::{spawn, JoinHandle};
 
-use anyhow::Result;
+use anyhow::{Result};
 use log::*;
-use os_pipe::{pipe, PipeReader};
+use os_pipe::{pipe, PipeReader, PipeWriter};
 
+/// Reader that reads from a background thread.
 pub struct ThreadRead {
   read: PipeReader,
+  handle: Option<JoinHandle<io::Result<u64>>>
+}
+
+/// Writer that writes to a background thread.
+pub struct ThreadWrite {
+  write: Option<PipeWriter>,
   handle: Option<JoinHandle<io::Result<u64>>>
 }
 
@@ -50,5 +58,69 @@ impl Read for ThreadRead {
       }
     }
     Ok(size)
+  }
+}
+
+impl ThreadWrite {
+  /// Push a read into a background thread.
+  pub fn new<W: Write + Send + 'static>(chan: W) -> Result<ThreadWrite> {
+    let (read, writer) = pipe()?;
+
+    let jh = spawn(move || {
+      let mut src = read;
+      let mut dst = chan;
+      copy(&mut src, &mut dst)
+    });
+
+    Ok(ThreadWrite {
+      write: Some(writer), handle: Some(jh)
+    })
+  }
+
+  fn do_close(&mut self) -> io::Result<u64> {
+    if let Some(mut write) = self.write.take() {
+      write.flush()?;
+      drop(write);
+    }
+    if let Some(h) = self.handle.take() {
+      let res = h.join().expect("thread error");
+      let sz = res?;
+      debug!("thread copied {} bytes", sz);
+      Ok(sz)
+    } else {
+      Err(io::ErrorKind::BrokenPipe.into())
+    }
+  }
+
+  /// Close and drop the thread writer.
+  pub fn close(mut self) -> io::Result<u64> {
+    self.do_close()
+  }
+}
+
+impl Drop for ThreadWrite {
+  fn drop(&mut self) {
+    if self.write.is_some() || self.handle.is_some() {
+      self.do_close().expect("unclosed background thread failed");
+    }
+  }
+}
+
+impl Write for ThreadWrite {
+  fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+    if let Some(ref mut write) = self.write {
+      write.write(buf)
+    } else {
+      Err(io::ErrorKind::BrokenPipe.into())
+    }
+  }
+
+  /// Flush the thread writer. This is not reliable.
+  fn flush(&mut self) -> io::Result<()> {
+    if let Some(ref mut write) = self.write {
+      write.flush()
+    } else {
+      Err(io::ErrorKind::BrokenPipe.into())
+    }
   }
 }
