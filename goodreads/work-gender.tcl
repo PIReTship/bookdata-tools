f4d51ef691db789e1fdd7abb756883d7bf451ddf
Michael Ekstrand <michaelekstrand@boisestate.edu>
2022-06-22 17:04:35 -0600

    convert one tcl to Python + Polars

--- goodreads/work-gender.tcl
+++
@@ -1,8 +0,0 @@
-table gender "../book-links/cluster-genders.parquet"
-table book_links "gr-book-link.parquet"
-
-save-results "gr-work-gender.parquet" {
-    SELECT DISTINCT work_id, cluster, gender
-    FROM book_links
-    JOIN gender USING (cluster)
-}
