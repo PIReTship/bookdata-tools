16e81fbba92dd86f476c6754b7b7802146c4ada9
Michael Ekstrand <michaelekstrand@boisestate.edu>
2022-06-23 10:13:51 -0600

    clean up Polars-based book hashing

--- src/cli/cluster/hash.rs
+++ src/cli/cluster/hash.rs
@@ -1,13 +1,14 @@
 //! Extract author information for book clusters.
 use std::path::{PathBuf};
-use std::fs::File;
-use std::sync::Arc;
+use std::collections::HashMap;
 
+use arrow2_convert::ArrowField;
 use structopt::StructOpt;
 use md5::{Md5, Digest};
 use hex;
 
 use polars::prelude::*;
+use crate::arrow::TableWriter;
 use crate::prelude::*;
 
 use crate::prelude::Result;
@@ -17,52 +18,65 @@ use crate::prelude::Result;
 /// Compute a hash for each cluster.
 pub struct HashCmd {
   /// Specify output file
-  #[structopt(short="o", long="output")]
+  #[structopt(short="o", long="output", name="FILE")]
   output: PathBuf,
+
+  /// Specify input file
+  #[structopt(name="ISBN_CLUSTERS")]
+  cluster_file: PathBuf,
 }
 
-fn hash_isbns(isbns: Series) -> Result<Series, PolarsError> {
-  let s2 = isbns.sort(false);
-  let s2 = s2.utf8()?;
-  let mut hash = Md5::default();
-  for elt in s2 {
-    if let Some(isbn) = elt {
-      hash.update(isbn.as_bytes());
-    }
-  }
-  let res = Series::new("isbn_hash", vec![hex::encode(hash.finalize())]);
-  Ok(res)
-}
-
-fn hash_field(_schema: &Schema, _ctx: Context, _fields: &[Field]) -> Field {
-  Field::new("isbn_hash", DataType::Utf8)
+#[derive(ArrowField)]
+struct ClusterHash {
+  cluster: i32,
+  isbn_hash: String,
+  isbn_dcode: i8,
 }
 
 /// Load ISBN data
-fn scan_isbns() -> Result<LazyFrame> {
-  let icl = LazyFrame::scan_parquet("isbn-clusters.parquet".to_string(), default())?;
+fn scan_isbns(path: &Path) -> Result<LazyFrame> {
+  let path = path.to_str().map(|s| s.to_string()).ok_or(anyhow!("invalid UTF8 pathname"))?;
+  info!("scanning ISBN cluster file {}", path);
+  let icl = LazyFrame::scan_parquet(path, default())?;
   let icl = icl.select(&[col("isbn"), col("cluster")]);
   Ok(icl)
 }
 
 impl Command for HashCmd {
   fn exec(&self) -> Result<()> {
-    let isbns = scan_isbns()?;
-    let hashes = isbns.groupby(&[col("cluster")]).agg(&[
-      col("isbn").apply(hash_isbns, NoEq::new(Arc::new(hash_field)))
-    ]);
+    let isbns = scan_isbns(self.cluster_file.as_path())?;
+
+    // It would be nice to do this with group-by, but group-by is quite slow and introduces
+    // unhelpful overhead. Sorting (for consistency) and a custom loop to aggregate ISBNs
+    // into hashes is much more efficient.
+    info!("reading sorted ISBNs into memory");
+    let isbns = isbns.sort("isbn", SortOptions::default()).collect()?;
 
     info!("computing ISBN hashes");
-    let mut hashes = hashes.collect()?;
-    let (nr, nc) = hashes.shape();
-    assert_eq!(nc, 2);
-    info!("computed hashes for {} clusters", nr);
+    let mut hashes: HashMap<i32, Md5> = HashMap::new();
+    let isbn_col = isbns.column("isbn")?.utf8()?;
+    let clus_col = isbns.column("cluster")?.i32()?;
+    for pair in isbn_col.into_iter().zip(clus_col.into_iter()) {
+      if let (Some(i), Some(c)) = pair {
+        hashes.entry(c).or_default().update(i.as_bytes());
+      }
+    }
+
+    info!("computed hashes for {} clusters", hashes.len());
 
     let path = self.output.as_path();
-    info!("saving Parquet to {:?}", path);
-    let file = File::create(path)?;
-    let write = ParquetWriter::new(file).with_compression(ParquetCompression::Zstd(None));
-    write.finish(&mut hashes)?;
+    info!("writing ISBN hashes to {:?}", path);
+    let mut writer = TableWriter::open(path)?;
+    for (cluster, h) in hashes.into_iter() {
+      let h = h.finalize();
+      writer.write_object(ClusterHash {
+        cluster,
+        isbn_hash: hex::encode(h),
+        isbn_dcode: (h[h.len() - 1] % 2) as i8
+      })?;
+    }
+
+    writer.finish()?;
 
     Ok(())
   }
