a9bbb80d00c7facaa6e77f7ca007ca09f1a76826
Michael Ekstrand <michaelekstrand@boisestate.edu>
2022-11-02 15:28:21 -0600

    support jemalloc

--- src/main.rs
+++ src/main.rs
@@ -23,10 +23,19 @@ mod interactions;
 mod cli;
 mod prelude;
 
+#[cfg(feature="mimalloc")]
 use mimalloc::MiMalloc;
 
+#[cfg(feature="mimalloc")]
 #[global_allocator]
 static GLOBAL: MiMalloc = MiMalloc;
+
+#[cfg(feature="jemalloc")]
+use jemalloc::Jemalloc;
+
+#[cfg(feature="jemalloc")]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
 
 use anyhow::Result;
 use structopt::StructOpt;
