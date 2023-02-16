a773c1a6985ac3238371abb21f3c6e35ca34ea71
mdekstrand <md@ekstrandom.net>
2023-02-16 01:14:37 +0000

    Deploying to gh-pages from @ BoiseState/bookdata-tools@b2bfdf7212d1cffa9ec62f3460aee395965f02d0 🚀

--- apidocs/implementors/fallible_iterator/trait.FallibleIterator.js
+++ apidocs/implementors/fallible_iterator/trait.FallibleIterator.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"bookdata":[["impl&lt;B:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.1/std/io/trait.BufRead.html\" title=\"trait std::io::BufRead\">BufRead</a>&gt; <a class=\"trait\" href=\"bookdata/prelude/trait.FallibleIterator.html\" title=\"trait bookdata::prelude::FallibleIterator\">FallibleIterator</a> for <a class=\"struct\" href=\"bookdata/marc/parse/struct.Records.html\" title=\"struct bookdata::marc::parse::Records\">Records</a>&lt;B&gt;"]],
+"bookdata":[["impl&lt;B:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.1/std/io/trait.BufRead.html\" title=\"trait std::io::BufRead\">BufRead</a>&gt; <a class=\"trait\" href=\"bookdata/prelude/trait.FallibleIterator.html\" title=\"trait bookdata::prelude::FallibleIterator\">FallibleIterator</a> for <a class=\"struct\" href=\"bookdata/marc/parse/struct.Records.html\" title=\"struct bookdata::marc::parse::Records\">Records</a>&lt;B&gt;"]],
 "fallible_iterator":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
