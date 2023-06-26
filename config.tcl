e58c45984ae98ad619e6682a765b2cc1a0c20775
Michael Ekstrand <michaelekstrand@boisestate.edu>
2023-06-26 15:33:29 -0600

    config

--- config.tcl
+++ config.tcl
@@ -1,3 +1,9 @@
+# consumption datasets can be enabled and disabled
+# TODO NOT YET SUPPORTED
+set datasets {
+
+}
+
 # change to "full" and re-render to use full GR interaction files instead of CSVs
 set gr_interactions simple
 # set gr_interactions full
