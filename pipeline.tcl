f379fae8cb15cc63d527d2de08bd468e7c1395ab
Michael Ekstrand <michaelekstrand@boisestate.edu>
2023-06-26 14:18:15 -0600

    fix schema

--- pipeline.tcl
+++ pipeline.tcl
@@ -2,7 +2,7 @@ namespace import ::plumber::list_stages
 namespace import ::plumber::stage_*
 
 subdir loc-mds
-# subdir openlibrary
+subdir openlibrary
 # subdir viaf
 # subdir az2014
 # subdir az2018
@@ -37,7 +37,7 @@ set parquets [list]
 foreach stage [list_stages] {
     foreach out [stage_outs $stage] {
         if {[string match *.parquet $out]} {
-            lappend parquets $out
+            lappend parquets [file rootname $out]
         }
     }
 }
