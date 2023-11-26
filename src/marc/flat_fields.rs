3ec35cad04b79e65e56abe3e8021a79822230fbb
Michael Ekstrand <mdekstrand@drexel.edu>
2023-11-26 15:38:28 +0000

    temporariliy revert flat field change

--- src/marc/flat_fields.rs
+++ src/marc/flat_fields.rs
@@ -1,12 +1,14 @@
 use anyhow::Result;
 use std::path::Path;
 
+use arrow2_convert::*;
+
 use super::record::*;
 use crate::arrow::*;
 use crate::io::*;
 
 /// Flat MARC field record.
-#[derive(TableRow, Debug, Default)]
+#[derive(ArrowField, ArrowSerialize, ArrowDeserialize, Debug, Default)]
 pub struct FieldRecord {
     pub rec_id: u32,
     pub fld_no: u32,
