889d6aecf0e1c7639a005377475871e84f4e8d76
Michael Ekstrand <mdekstrand@drexel.edu>
2023-12-11 12:42:28 -0500

    support schema shortcode

--- docs/_extensions/doclinks/files.lua
+++ docs/_extensions/doclinks/files.lua
@@ -1,7 +1,7 @@
 function load_overrides()
     local path, f, text
     if overrides == nil then
-        path = pandoc.path.join({quarto.project.directory, "_data-overrides.json"})
+        path = pandoc.path.join({ quarto.project.directory, "_data-overrides.json" })
         f = io.open(path)
         if f == nil then
             quarto.log.warning("could not open data overrides")
@@ -12,6 +12,23 @@ function load_overrides()
         overrides = quarto.json.decode(text)
         quarto.log.debug(overrides)
     end
+end
+
+function load_schema(file)
+    local path, base, ext, f, text
+    path = pandoc.path.join({ quarto.project.directory, "..", file })
+    base, ext = pandoc.path.split_extension(path)
+    path = base .. ".json"
+    quarto.log.debug("reading schema file", path)
+
+    f = io.open(path)
+    if f == nil then
+        quarto.log.warning("could not open schema file", path)
+        return nil
+    end
+
+    text = f:read("a")
+    return quarto.json.decode(text)
 end
 
 return {
@@ -50,5 +67,36 @@ return {
         link = string.gsub(link, "(.+)#(.*)", "/data/%1.qmd#file:%2")
 
         return pandoc.Link(pandoc.Code(path), link)
+    end,
+
+    ['schema'] = function(args, kwargs, meta)
+        local path, schema, rows
+        path = pandoc.utils.stringify(args[1])
+        schema = load_schema(path)
+        if schema == nil then
+            return pandoc.Div({
+                pandoc.Str("Could not find schema file "),
+                pandoc.Code(path),
+                pandoc.Str(".")
+            })
+        end
+
+        header = pandoc.Row({
+            pandoc.Cell({ pandoc.Div("Field") }),
+            pandoc.Cell({ pandoc.Div("Type") }),
+        })
+        rows = pandoc.List()
+        for i, field in ipairs(schema.fields) do
+            rows[#rows + 1] = pandoc.Row({
+                pandoc.Cell({ pandoc.Div(field.name) }),
+                pandoc.Cell({ pandoc.Div(field.data_type) }),
+            })
+        end
+
+        local caption = {"Schema for ", pandoc.Code(path), "."}
+        return pandoc.Table({ long = { pandoc.Div({caption}) }, short = caption },
+            { { pandoc.AlignLeft, 60 }, { pandoc.AlignRight, 40 } },
+            pandoc.TableHead({ header }),
+            { { attr = pandoc.Attr(), body = rows, head = {}, row_head_columns = 0 } }, pandoc.TableFoot(), pandoc.Attr())
     end,
 }
