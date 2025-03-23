4c0655c8331758dd6115f962ff8ceb8eac7bc436
Michael Ekstrand <mdekstrand@drexel.edu>
2025-03-23 16:37:54 +0000

    move pipeline update into invoke

---
+++ tasks.py
@@ -0,0 +1,43 @@
+import json
+import os
+import sys
+from pathlib import Path
+
+from _jsonnet import evaluate_file
+from invoke.tasks import task
+from yaml import safe_dump
+
+root = Path(__file__).parent
+
+
+def msg(fmt, *args, **kwargs):
+    message = fmt.format(*args, **kwargs)
+    print(message, file=sys.stderr)
+
+
+def err(fmt, *args, **kwargs):
+    message = fmt.format(*args, **kwargs)
+    print("ERROR:", message, file=sys.stderr)
+
+
+@task
+def render_pipeline(c, dir=None, stdout=False):
+    "Re-render the DVC pipeline."
+
+    if dir:
+        specs = [Path(dir) / "dvc.jsonnet"]
+    else:
+        specs = root.glob("**/dvc.jsonnet")
+
+    if not specs:
+        err("no spec files found")
+
+    for path in specs:
+        msg("rendering {}", path)
+        results = evaluate_file(os.fspath(path))
+        results = json.loads(results)
+        if stdout:
+            safe_dump(results, sys.stdout, width=200)
+        else:
+            with path.with_suffix(".yaml").open("w") as pf:
+                safe_dump(results, pf, width=200)
